d2751192c6c3ce7aa9f4b1fc89ca770bef14a1d6
IndianSmartCoder <ergovindray@gmail.com>
2024-11-20 04:55:23 +0530

    Hi Coder . i have started a initiative to Code 360 Days as 360DegreeCoder. i love you all . "Every small step you take brings you closer to your greatest success." 🌟          It's a powerful reminder that consistent efforts, no matter how small, lead to big achievements. Keep moving forward! 🚀

---
+++ src/com/smartcoder/basicpart1/Hello.java
@@ -0,0 +1,13 @@
+package com.smartcoder.basicpart1;
+/*
+ Write a Java program to print 'Hello' on screen and your name on a separate line.
+Expected Output :
+Hello
+Govind Kumar Ray
+ */
+public class Hello {
+    public static void main (String[] args){
+        System.out.print("Hello"+" "+"\n Govind Kumar Ray");
+    }
+
+}
